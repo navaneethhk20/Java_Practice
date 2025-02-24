2f804179a6e2e74972643c2ced430bca866f8222
navaneethhhk <hknavaneeth@gmail.com>
2025-02-24 07:13:57 +0530

    commit

---
+++ src/Ex_practive24022025/Lab01_basicAirthmenticOperations.java
@@ -0,0 +1,15 @@
+package Ex_practive24022025;
+
+public class Lab01_basicAirthmenticOperations {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        System.out.println(2%2);
+      //  System.out.println(2/0); - ArithmeticException
+        int age =65;
+         age = 5;
+        System.out.println(age);
+    }
+}
