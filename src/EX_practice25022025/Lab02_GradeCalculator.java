8a9d0beab4611b6a0596f993eedc3b677cecb726
navaneethhhk <hknavaneeth@gmail.com>
2025-02-25 05:44:29 +0530

    commit25

--- src/EX_practice25022025/Lab02_GradeCalculator.java
+++ src/EX_practice25022025/Lab02_GradeCalculator.java
@@ -26,6 +26,8 @@ public class Lab02_GradeCalculator {
 
             grade = marks>90 && marks<=100?A:(marks>80&&marks<=89)?B:(marks>70&&marks<=79)?C:(marks>60&&marks<=69)?D:(marks>50&&marks<=59)?E:F;
             System.out.println(grade);
+
+            sc.close();
         }
     }
 
---
+++ src/EX_practice25022025/Lab03_MaximumOf2Numbers.java
@@ -0,0 +1,17 @@
+package EX_practice25022025;
+
+import java.util.Scanner;
+
+public class Lab03_MaximumOf2Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number");
+        int num2= sc.nextInt();
+
+        int maxnumber = num1>num2?num1:num2;
+        System.out.println("Maximum number is "+maxnumber);
+        sc.close();
+    }
+}
