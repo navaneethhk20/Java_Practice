1f039685da6c8e47a7413235d0244ae030521659
navaneethhhk <hknavaneeth@gmail.com>
2025-04-08 20:41:44 +0530

    commit

---
+++ src/EX08042025/Lab01.java
@@ -0,0 +1,14 @@
+package EX08042025;
+
+import java.util.Scanner;
+
+public class Lab01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2= sc.nextInt();
+        System.out.println("Enter the numbers");
+        int add = num1+num2;
+        System.out.println(add);
+    }
+}
