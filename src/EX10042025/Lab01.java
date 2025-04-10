67553222a5a131e64873224cc11efba201374392
navaneethhhk <hknavaneeth@gmail.com>
2025-04-10 06:39:41 +0530

    commit

---
+++ src/EX10042025/Lab01.java
@@ -0,0 +1,14 @@
+package EX10042025;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        int a =10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
---
+++ src/EX10042025/Lab02.java
@@ -0,0 +1,12 @@
+package EX10042025;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        String name = "nithu";
+        System.out.println(a+b+name);
+        System.out.println(name+(a+b));
+        System.out.println(a+name+b);
+    }
+}
